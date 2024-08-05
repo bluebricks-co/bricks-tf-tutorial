066ff3e0061d3336a9fe43f366b42d74c29a3504
Idan Yalovich <idan.yalovich@gmail.com>
2024-08-05 13:26:33 +0300

    region removed

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "bluebricks_tutorial" {
   bucket = "tutorial-bricks-cli"
-  region = "eu-north-1"
   tags = {
     Name        = "Created by bricks cli"
     Environment = "Dev"
